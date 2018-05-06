e410b112e570042797938e898f01e97a9796cc24
Rogerio Chaves <rogeriocfj@gmail.com>
2018-05-05 23:26:51 -0300

    add all functions to transform the package-lock file

--- src/npm_force_resolutions/core.cljs
+++ src/npm_force_resolutions/core.cljs
@@ -1,3 +1,58 @@
-(ns npm-force-resolutions.core)
+(ns npm-force-resolutions.core
+  (:require [cljs.nodejs :as nodejs]
+            [cognitect.transit :as t]))
 
-(println "Hello world!")
+(defn main [& args]
+  (str "args: " (first args)))
+
+(defn node-slurp [path]
+  (let [fs (nodejs/require "fs")]
+    (.readFileSync fs path "utf8")))
+
+(defn read-json [path]
+  (t/read (t/reader :json) (node-slurp path)))
+
+(defn find-resolutions [folder]
+  (let [package-json (read-json (str folder "/package.json"))]
+    (get package-json "resolutions")))
+
+(defn remove-from-requires [resolutions dependency]
+  (update dependency "requires" #(apply dissoc % (keys resolutions))))
+
+; Source: https://stackoverflow.com/questions/1676891/mapping-a-function-on-the-values-of-a-map-in-clojure
+(defn map-vals [f m]
+  (apply merge (map (fn [[k v]] {k (f k v)}) m)))
+
+(defn add-dependencies [resolutions dependency]
+  (let [required-dependencies (keys (get dependency "requires"))
+        new-dependencies (select-keys
+                          (map-vals (fn [k v] {"version" v}) resolutions)
+                          required-dependencies)
+        with-deps (merge-with into dependency {"dependencies" {}})]
+    (update with-deps "dependencies" #(conj % new-dependencies))))
+
+(defn fix-existing-dependency [resolutions key dependency]
+  (if (contains? resolutions key)
+    (conj (dissoc dependency "version" "resolved" "integrity" "bundled") {"version" (get resolutions key)})
+    dependency))
+
+(defn patch-dependency [resolutions key dependency]
+  (if (contains? dependency "requires")
+    (->> dependency
+        (add-dependencies resolutions)
+        (remove-from-requires resolutions)
+        (fix-existing-dependency resolutions key)
+        (patch-all-dependencies resolutions))
+    (fix-existing-dependency resolutions key dependency)))
+
+(defn patch-all-dependencies [resolutions package-lock]
+  (update package-lock "dependencies"
+    (fn [dependencies]
+      (map-vals #(patch-dependency resolutions %1 %2) dependencies))))
+
+(defn update-package-lock [folder]
+  (let [package-lock (read-json (str folder "/package-lock.json"))
+        resolutions (find-resolutions folder)]
+    (patch-all-dependencies resolutions package-lock)))
+
+(set! *main-cli-fn* main)
---
+++ src/npm_force_resolutions/core_test.cljs
@@ -0,0 +1,121 @@
+(ns npm-force-resolutions.core-test
+  (:require [cljs.test :refer-macros [deftest is testing run-tests]]
+            [npm-force-resolutions.core :refer [main node-slurp read-json find-resolutions
+                                                patch-all-dependencies remove-from-requires
+                                                add-dependencies update-package-lock
+                                                fix-existing-dependency]]))
+
+(deftest test-args
+  (is (= (main "foo") "args: foo")))
+
+(deftest test-read-file
+  (let [package-lock-file (node-slurp "./src/fixtures/package-lock.json")]
+    (is (re-find #"package-lock-fixture-before" package-lock-file))))
+
+(deftest test-read-package-lock-json
+  (let [package-lock (read-json "./src/fixtures/package-lock.json")]
+    (is (= (get package-lock "name") "package-lock-fixture-before"))))
+
+(deftest test-find-resolutions
+  (let [resolutions (find-resolutions "./src/fixtures")]
+    (is (= resolutions
+          {"hoek" "4.2.1"
+           "example" "1.0.0"}))))
+
+(deftest test-remove-from-require
+  (let [resolutions (find-resolutions "./src/fixtures")
+        dependency {"requires" {"hoek" "1.0.0"}}
+        updated-dependency (remove-from-requires resolutions dependency)]
+    (is (= updated-dependency
+          {"requires" {}}))))
+
+(deftest test-remove-requires
+  (let [resolutions (find-resolutions "./src/fixtures")
+        package-lock (read-json "./src/fixtures/package-lock.json")
+        updated-package-lock (patch-all-dependencies resolutions package-lock)]
+    (is (= {}
+          (-> updated-package-lock
+            (get "dependencies")
+            (get "boom")
+            (get "requires"))))))
+
+(deftest test-remove-requires-recursivelly
+  (let [resolutions (find-resolutions "./src/fixtures")
+        package-lock (read-json "./src/fixtures/package-lock.json")
+        updated-package-lock (patch-all-dependencies resolutions package-lock)]
+    (is (= {}
+          (-> updated-package-lock
+            (get "dependencies")
+            (get "fsevents")
+            (get "dependencies")
+            (get "boom")
+            (get "requires"))))))
+
+(deftest test-add-dependencies-if-there-is-require
+  (let [resolutions (find-resolutions "./src/fixtures")
+        dependency {"requires" {"hoek" "1.0.0"}
+                    "dependencies" {"foo" {"version" "2.0.0"}}}
+        updated-dependency (add-dependencies resolutions dependency)]
+    (is (= updated-dependency
+          {"requires" {"hoek" "1.0.0"}
+           "dependencies" {"foo" {"version" "2.0.0"}
+                           "hoek" {"version" "4.2.1"}}}))))
+
+(deftest test-add-dependencies-if-there-is-require-and-no-dependencies
+  (let [resolutions (find-resolutions "./src/fixtures")
+        dependency {"requires" {"hoek" "1.0.0"}}
+        updated-dependency (add-dependencies resolutions dependency)]
+    (is (= updated-dependency
+          {"requires" {"hoek" "1.0.0"}
+           "dependencies" {"hoek" {"version" "4.2.1"}}}))))
+
+(deftest test-do-not-add-dependencies-if-there-is-no-require
+  (let [resolutions (find-resolutions "./src/fixtures")
+        dependency {"requires" {}
+                    "dependencies" {"foo" {"version" "2.0.0"}}}
+        updated-dependency (add-dependencies resolutions dependency)]
+    (is (= updated-dependency
+          {"requires" {}
+           "dependencies" {"foo" {"version" "2.0.0"}}}))))
+
+(deftest test-add-dependencies-recursivelly
+  (let [resolutions (find-resolutions "./src/fixtures")
+        package-lock (read-json "./src/fixtures/package-lock.json")
+        updated-package-lock (patch-all-dependencies resolutions package-lock)]
+    (is (= {"hoek" {"version" "4.2.1"}}
+          (-> updated-package-lock
+            (get "dependencies")
+            (get "fsevents")
+            (get "dependencies")
+            (get "boom")
+            (get "dependencies"))))))
+
+(deftest test-fix-existing-dependency
+  (let [resolutions (find-resolutions "./src/fixtures")
+        dependency {"version" "2.16.3"
+                    "resolved" "https://registry.npmjs.org/hoek/-/hoek-2.16.3.tgz"
+                    "integrity" "sha1-ILt0A9POo5jpHcRxCo/xuCdKJe0="
+                    "dev" true}
+        updated-dependency (fix-existing-dependency resolutions "hoek" dependency)]
+    (is (= updated-dependency
+          {"version" "4.2.1"
+           "dev" true}))))
+
+(deftest test-does-not-fix-existing-dependency-that-is-not-on-resolutions
+  (let [resolutions (find-resolutions "./src/fixtures")
+        dependency {"version" "2.16.3"
+                    "resolved" "https://registry.npmjs.org/hoek/-/hoek-2.16.3.tgz"
+                    "integrity" "sha1-ILt0A9POo5jpHcRxCo/xuCdKJe0="
+                    "dev" true}
+        updated-dependency (fix-existing-dependency resolutions "foo" dependency)]
+    (is (= updated-dependency
+          dependency))))
+
+(deftest test-update-package-lock
+  (let [expected-package-lock (read-json "./src/fixtures/package-lock.after.json")
+        updated-package-lock (update-package-lock "./src/fixtures")]
+    (is (= (get expected-package-lock "dependencies")
+           (get updated-package-lock "dependencies")))))
+
+(enable-console-print!)
+(run-tests)
